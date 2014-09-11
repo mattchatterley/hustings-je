407107eda227c29cbac1abea34cb804f47264040
mattchatterley <matt.chatterley@gmail.com>
2014-09-11 16:41:59 +0100

    Create test harness for Engine

---
+++ www/test/EngineTest.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(E_ALL | E_STRICT);
+ini_set('display_errors', 1);
+
+include_once('../inc/database.php');
+include_once('../inc/User.php');
+include_once('../inc/Engine.php');
+
+//var_dump($_POST);
+
+echo("Starting...");
+
+// get participants
+$users = User::All();
+
+$from = date('Y-m-d H:i:s', 0);
+$to = date('Y-m-d H:i:s', time());
+$timeslot = 'day';
+$dataset = 'sentiment-over-time';
+
+echo("Calling Engine...");
+
+// compute results
+$engine = new Engine($users, $from, $to, $timeslot, $dataset);
+$engine->Compute();
+
+var_dump($engine->Results);
+// use json_encode to output results
+
+//var_dump($engine->Results);
+echo(json_encode($engine->Results));
+die();
+?>
